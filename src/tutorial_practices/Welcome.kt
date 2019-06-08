0db9ee632fe8aff627b47dfbfdded5552ab4c408
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-06-08 20:47:23 +0600

    main() function calling,getting output and cattenation

---
+++ src/tutorial_practices/Welcome.kt
@@ -0,0 +1,20 @@
+package tutorial_practices
+
+// To declare function ...fun.... keyword is used. if fun is only used-
+//then the return type will be void and access modifier will be public.
+// In ...()... args ->variable name, Array<String>-- Data type
+
+
+fun main(args:Array<String>){
+
+    //Simply getting output by .. println()...
+    println("Nilotpol is a good boy..")
+
+    var a = 5
+    var b = 6
+
+    // In kotlin following two types catennation is possible and output is same.
+    println("the summation of "+a+" and "+b+" is: "+a+b)
+    println("the summation of $a and $b is: ${a+b}")
+
+}
