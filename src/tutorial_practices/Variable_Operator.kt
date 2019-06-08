026206594659e3cd97bbaea9a351aa4435bf56b7
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-06-08 23:25:21 +0600

    variable type,when loop and for loop

---
+++ src/tutorial_practices/Variable_Operator.kt
@@ -0,0 +1,30 @@
+package tutorial_practices
+
+fun main(args: Array<String>){
+    /*data types ->majorly 2 types a)mutable(var) b)immutable(val)
+    var type variable's value can be changed bur val type variable's value can not
+    be changed (it is like final keyword of java)
+    Besides,Object data type(Integer,Long,String etc.) is available
+    .declaration system like :var/val variableName:dataType = value.*/
+    val a = 5
+    val b:Int = 7 //best practice
+
+    //b = 8 not possible, a = 8 possible
+
+    //Use of for loop for n times
+    for(i in 0..49){  //here 0 to 49 , n=50
+        print("$i,")  //to stay in same line
+    }
+    println("")
+
+    //Use of When loop. It is almost same as ..switch.. statement
+
+    when(a){
+        1-> println("You are First")
+        2->println("you are second")
+        5->println("you are fifth")
+        else -> println("a not equal to 1,2,5")
+    }
+
+
+}
